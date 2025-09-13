2ea8a6c6bdb3962402e0b3b5148cf726e23f46b0
crisMdn <Croow2507@gmail.com>
2025-09-12 18:07:31 -0600

    actualizacion

--- postgre/createdb.sql
+++ postgre/createdb.sql
@@ -3,10 +3,3 @@ CREATE USER canelito WITH PASSWORD '1234';
 GRANT ALL PRIVILEGES ON DATABASE Botton TO canelito;
 
 \c botton
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-CREATE TABLE images (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  filename TEXT NOT NULL,
-  url TEXT NOT NULL,
-  uploaded_at TIMESTAMPTZ DEFAULT now()
-);
